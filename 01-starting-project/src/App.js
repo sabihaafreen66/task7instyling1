3e1e942f8fc086d78059264268cd8021cb9cbb8b
SabihaAfreen <afrah9059@gmail.com>
2024-05-06 16:56:38 +0530

    fff

--- 01-starting-project/src/App.js
+++ 01-starting-project/src/App.js
@@ -1,42 +1,20 @@
-import React, { useState,useEffect} from 'react';
-import AuthContext from './store/auth-context';
+import React ,{useContext}from 'react';
+
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
+import AuthContext from './store/auth-context';
 
 function App() {
-useEffect(()=>{
-  const storedUserLoggedInInformation=localStorage.getItem('isLoggedIn');
-  if(storedUserLoggedInInformation==='1'){
-    setIsLoggedIn(true);
-  }
-},[]);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  const loginHandler = (email, password,collegName) => {
-     localStorage.setItem('isLoggedIn','1');
-       setIsLoggedIn(true);
-  };
-
-  const logoutHandler = () => {
-    localStorage.removeItem('isLoggedIn');
-    setIsLoggedIn(false);
-  };
-
+const ctx=useContext(AuthContext);
   return (
     <React.Fragment>
-      <AuthContext.Provider
-      value={{
-        isLoggedIn:isLoggedIn,
-        onLogout:logoutHandler
-        }}>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+           <MainHeader  />
       <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
+        {!ctx.isLoggedIn && <Login />}
+        {ctx.isLoggedIn && <Home/>}
       </main>
-      </AuthContext.Provider>
-    </React.Fragment>
+         </React.Fragment>
   );
 }
 
--- 01-starting-project/src/components/Login/Login.js
+++ 01-starting-project/src/components/Login/Login.js
@@ -1,7 +1,9 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useState, useEffect, useReducer,useContext } from 'react';
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from '../../store/auth-context';
+import Input from '../UI/Input/Input';
 
 const Login = (props) => {
 
@@ -19,6 +21,9 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   const[emailState,dispatchEmail]=useReducer(emailReducer,{value:'',isValid:false});
+
+const authctx=useContext(AuthContext)
+
 
    useEffect(()=>{
     console.log('EFFECT RUNNING')
@@ -59,26 +64,16 @@ const Login = (props) => {
   const submitHandler = (event) => {
     event.preventDefault();
     if (formIsValid) {
-      props.onLogin(enteredEmail, enteredPassword, enteredCollegeName);
+      authctx.onLogin(enteredEmail, enteredPassword, enteredCollegeName);
     }
   };
 
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
-        <div
-          className={`${classes.control} ${
-            emailIsValid === false ? classes.invalid : ''
-          }`}
-        >
-          <label htmlFor="email">E-Mail</label>
-          <input
-            type="email"
-            id="email"
-            value={enteredEmail}
-            onChange={emailChangeHandler}
-          />
-        </div>
+       <Input id="email"label="E-Mail" type={emailIsValid}
+        value={emailState.value}
+                       />
         <div
           className={`${classes.control} ${
             passwordIsValid === false ? classes.invalid : ''
